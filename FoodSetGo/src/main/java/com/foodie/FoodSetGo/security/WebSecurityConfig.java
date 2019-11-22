7a4a1727cb3851cef8dc293cfc567fbc85970bcb
trinitipsy <jankovic-tamara@hotmail.com>
2019-11-22 19:11:18 +0100

    Add WebSecurityCongig

---
+++ FoodSetGo/src/main/java/com/foodie/FoodSetGo/security/WebSecurityConfig.java
@@ -0,0 +1,38 @@
+package com.foodie.FoodSetGo.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.authorizeRequests()
+                //USER PATHS
+                .antMatchers(HttpMethod.POST, "/users").permitAll()
+                .antMatchers(HttpMethod.POST, "/login").permitAll() //DODATI ENDPOINT
+
+
+                .antMatchers(HttpMethod.DELETE, "/users/**").hasRole("USER")
+                .antMatchers(HttpMethod.PUT, "/users/**").hasRole("USER")
+                .antMatchers(HttpMethod.POST, "/orders").hasRole("USER")
+                .antMatchers(HttpMethod.GET, "/users/**").hasRole("ADMIN")
+
+                .antMatchers(HttpMethod.PUT, "/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.POST, "/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.DELETE, "/**").hasRole("ADMIN")
+                .anyRequest().authenticated();
+
+    }
+}
