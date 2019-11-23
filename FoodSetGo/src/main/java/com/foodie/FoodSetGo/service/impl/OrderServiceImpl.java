96fc4dd3ba71501ccf26e637a5f5a1fc034a5903
trinitipsy <jankovic-tamara@hotmail.com>
2019-11-23 20:58:48 +0100

    Optimize imports

--- FoodSetGo/src/main/java/com/foodie/FoodSetGo/service/impl/OrderServiceImpl.java
+++ FoodSetGo/src/main/java/com/foodie/FoodSetGo/service/impl/OrderServiceImpl.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Service;
 
 import java.util.Collections;
 import java.util.List;
-import java.util.function.Function;
 import java.util.function.ToDoubleFunction;
 
 @Service
