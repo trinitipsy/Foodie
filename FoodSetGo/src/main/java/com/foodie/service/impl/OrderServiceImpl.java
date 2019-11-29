0d394daea310ea8f2018fc6adbd5b333c7d8c739
trinitipsy <jankovic-tamara@hotmail.com>
2019-11-29 13:39:55 +0100

    Style code

--- FoodSetGo/src/main/java/com/foodie/service/impl/OrderServiceImpl.java
+++ FoodSetGo/src/main/java/com/foodie/service/impl/OrderServiceImpl.java
@@ -21,31 +21,18 @@ import java.util.stream.Collectors;
 public class OrderServiceImpl implements OrderService {
     private final OrderRepository orderRepository;
     private final FoodRepository foodRepository;
-    private final OrderItemRepository orderItemRepository;
 
     @Override
     public Order add(String address, CreateOrderRequest createOrderRequest) {
         List<Integer> foodIds = createOrderRequest.getFoodIds();
         List<Food> food = foodRepository.findAllById(foodIds);
-        /*List<OrderItem> orderItems = new ArrayList<>();
-        Double totalPrice = 0d; */
         Order order = new Order();
-        //order.setOrderItem(orderItems);
 
         List<OrderItem> orderItems1 = food.stream().map(f -> new OrderItem(f, Collections.frequency(foodIds, f.getId()), order, f.getPrice())).collect(Collectors.toList());;
         Double totalPrice1 = orderItems1.stream().mapToDouble(foodItem -> foodItem.getFood().getPrice() * foodItem.getAmount()).sum();
 
         order.setOrderItem(orderItems1);
         order.setTotalPrice(totalPrice1);
-
-       /* for (Food x : food) {
-            Integer amount = Collections.frequency(foodIds, x.getId());
-            Double price = x.getPrice() * amount;
-            totalPrice += price;
-            orderItems.add(new OrderItem(x, amount, order, price));
-        }
-
-        order.setTotalPrice(totalPrice);*/
         order.setDeliveryAddress(address);
 
         return orderRepository.save(order);
